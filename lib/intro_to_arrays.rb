a3a0e71ccdee0a038d7bd6954906aec2e0dae245
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:38:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,8 +2,10 @@ def instantiate_new_array
   []
 end
 
-def array_with_two_elements
-  ["Westley", "Vivi"]
+def array_with_two_elements(array)
+  array = ["Westley", "Vivi"]
 end
 
-def 
+def first_element(array)
+array =  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+end
