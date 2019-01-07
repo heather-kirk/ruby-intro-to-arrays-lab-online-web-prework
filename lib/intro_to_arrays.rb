06785bc7e8bdaebefcde35c591094b43ababb8b7
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:41:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,4 +22,12 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array = 
+  array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.last
+end
+
